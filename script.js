6d47ab7dad2538b1436793739be3a33711c4fc6d
bella-s-4 <songi0085@my.sduhsd.net>
2026-04-10 09:19:03 +0000

    fixing code/debugging

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ let answer = 0;
 let guessCount = 0;
 let totalWins = 0;
 let totalGuesses = 0;
-let scores = 0;
+let scores = [];
+
 
 //Player Name
 let playerName = prompt("Enter your name:");
 //***Remember to check name and change capitalizations and stuff */
 
-function titleCase(playerName) {
-  let first = word.charAt(0).toUpperCase();
-  let rest = word.slice(1).toLowerCase();
-  let playerName = first + rest;
+function titleCase(name) {
+  let first = name.charAt(0).toUpperCase();
+  let rest = name.slice(1).toLowerCase();
+  return first + rest;
 }
+
+playerName = titleCase(playerName);
 
 //date 
 
@@ -25,23 +28,22 @@ let day = now.getDate();
 let year = now.getFullYear();
 alert(month + "/" + day + "/" + year);  // e.g. "4/7/2026"
 
-//timer 
+//timer or clock
 
-let seconds = 0;
-
-let timer = setInterval(function() {
-  seconds++;
-  document.getElementById("clock").innerHTML = seconds + "s";
-}, 1000);  // Runs every 1000 milliseconds (1 second)
+setInterval(function(){
+  document.getElementById("date").textContent = time();
+},1000);
 
 //Play
 //get level
-document.getElementById("playBtn").addEventListener("click", function() {
+document.getElementById("playBtn").addEventListener("click", play);
+
+function play() {
     let radios = document.getElementsByName("level");
     let range = 3;
     for (let i=0; i < radios.length; i++) {
         if(radios[i].checked) {
-            range = parseint(radios[i].value);
+            range = parseInt(radios[i].value);
         }
     }
 
@@ -49,22 +51,26 @@ document.getElementById("playBtn").addEventListener("click", function() {
 
     answer = Math.floor(Math.random() * range) + 1;
     guessCount = 0; //reset guess count for new round
+
+
     //Disable and Enable buttons and radio choices
     document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
+    
     document.getElementById("guess").value="";
     document.getElementById("guessBtn").disabled = false;
     document.getElementById("giveUpBtn").disabled = false;
     document.getElementById("playBtn").disabled = true;
 
-    let levelRadios = document.getElementsByName("level");
-    for (let i = 0; i < levelRadios.length; i++) {
-        levelRadios[i].disabled = true;
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].disabled = true;
     }
 
-});
+};
 
 //Guessing 
-document.getElementById("guessBtn").addEventListener("click", function() {
+document.getElementById("guessBtn").addEventListener("click", makeGuess);
+
+function makeGuess() {
     let input = document.getElementById("guess").value;
     let num = parseInt(input);
     if (isNaN(num)) {
@@ -80,7 +86,7 @@ document.getElementById("guessBtn").addEventListener("click", function() {
     if(num === answer){
         document.getElementById("msg").textContent = "Correct! " + playerName + " got it in " + guessCount + " guesses!";
         updateScore(guessCount);
-        resetButtons(); //stop guess & give up restart play
+        reset(); //stop guess & give up restart play
     }
     //higher
     else if (num > answer) {
@@ -106,7 +112,7 @@ document.getElementById("guessBtn").addEventListener("click", function() {
         }
         document.getElementById("msg").textContent = "Too low. " + temp;
     }
-})
+}
 
 //update score when win 
 
@@ -122,7 +128,7 @@ function updateScore(score) {
     scores.sort(function(a,b){return a-b;});
 
     let leaderboard = document.getElementsByName("leaderboard");
-    for (let i=0; i < leaderboard/length; i++) {
+    for (let i=0; i < leaderboard.length; i++) {
         if (i < scores.length) {
             leaderboard[i].textContent = scores[i];
         } else {
